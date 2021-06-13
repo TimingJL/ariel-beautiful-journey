a4c69e9cc8bf68d0ce1915269e083a6fcdeb84a7
taiming <taiming@bridgewell.com>
2021-06-13 12:10:34 +0800

    Add ability to upload cover image

--- src/pages/blog/blogFormDialog.js
+++ src/pages/blog/blogFormDialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -11,6 +11,7 @@ import Slide from '@material-ui/core/Slide';
 import { toastShow } from 'src/components/toastShow';
 import RichTextEditor from 'src/components/richTextEditor';
 import TextField from '@material-ui/core/TextField';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -23,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
   contentWrapper: {
     padding: 20,
+    paddingBottom: 80,
     display: 'flex',
     justifyContent: 'center',
   },
@@ -35,16 +37,40 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     maxWidth: 992,
   },
+  uploadButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    width: 160,
+    height: 90,
+    borderRadius: 4,
+    border: '2px dotted #ddd',
+    '&:hover': {
+      background: '#eee',
+    },
+  },
+  resetUploadButton: {
+    border: '1px solid #8b8bff',
+    color: '#8b8bff',
+    marginTop: 8,
+  },
+  previewImage: {
+    maxWidth: 300,
+    maxHeight: 300,
+  },
 }));
 
 const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
 
 const BlogFormDialog = ({ title, isOpen, handleClose }) => {
   const classes = useStyles();
-  const [value, setValue] = useState('');
+  const inputFileRef = useRef(null);
+  const [blogContent, setBlogContent] = useState('');
+  const [coverLink, setCoverLink] = useState('');
 
   const handleClickSaveButton = () => {
-    console.log('value: ', value);
     toastShow({
       type: 'success',
       message: '儲存成功！',
@@ -52,7 +78,29 @@ const BlogFormDialog = ({ title, isOpen, handleClose }) => {
   };
 
   const handleOnEditorChange = (textValue) => {
-    setValue(textValue);
+    setBlogContent(textValue);
+  };
+
+  const handleClickUploadButton = () => {
+    inputFileRef.current.click();
+  };
+
+  const handleOnSelectImage = (event) => {
+    const formData = new FormData();
+    formData.append('image', event.target.files[0]);
+
+    fetch('https://api.imgur.com/3/image/', {
+      method: 'post',
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_IMGUR_CLIENT_ID}`,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        const { data: { link } } = response;
+        setCoverLink(link);
+      });
   };
 
   return (
@@ -72,7 +120,7 @@ const BlogFormDialog = ({ title, isOpen, handleClose }) => {
       </AppBar>
       <div className={classes.contentWrapper}>
         <div className={classes.formGroup}>
-          <div>
+          <div className="blog-title">
             <div className={classes.label}>標題：</div>
             <TextField
               autoFocus
@@ -82,10 +130,43 @@ const BlogFormDialog = ({ title, isOpen, handleClose }) => {
               onChange={() => null}
             />
           </div>
-          <div>
+          <div className="cover-image-upload">
+            <div className={classes.label}>封面圖片：</div>
+            {coverLink ? (
+              <div>
+                <div>
+                  <img src={coverLink} alt="" className={classes.previewImage} />
+                </div>
+                <Button
+                  className={classes.resetUploadButton}
+                  onClick={handleClickUploadButton}
+                >
+                  <AddIcon />
+                  重新上傳
+                </Button>
+              </div>
+            ) : (
+              <Button
+                className={classes.uploadButton}
+                onClick={handleClickUploadButton}
+              >
+                <AddIcon />
+                上傳圖片
+              </Button>
+            )}
+            <input
+              ref={inputFileRef}
+              type="file"
+              onChange={handleOnSelectImage}
+              style={{ display: 'none' }}
+            />
+            {coverLink && <a href={coverLink}>{coverLink}</a>}
+          </div>
+          <div className="blog-content">
             <div className={classes.label}>內容：</div>
             <RichTextEditor handleOnChange={handleOnEditorChange} />
           </div>
+          <div style={{ display: 'none' }}>{blogContent}</div>
         </div>
       </div>
     </Dialog>
