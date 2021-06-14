11b21035882567fb1d888dad19381dff39023e75
taiming <taiming@bridgewell.com>
2021-06-14 19:49:34 +0800

    Add blog content paper

--- src/pages/blog/blogDetailPage/index.js
+++ src/pages/blog/blogDetailPage/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 
-const BlogDetailPage = () => (
-  <div>BlogDetailPage</div>
-);
+const useStyles = makeStyles({
+  container: {
+    padding: '20px 8px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  contentPaper: {
+    background: 'white',
+    width: '100%',
+    maxWidth: 900,
+    borderRadius: 4,
+    minHeight: 400,
+    boxShadow: '0px 0px 12px 0px #0000001F',
+  },
+});
+
+const BlogDetailPage = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <div className={classes.contentPaper}>
+        BlogDetailPage
+      </div>
+    </Container>
+  );
+};
 
 export default BlogDetailPage;
