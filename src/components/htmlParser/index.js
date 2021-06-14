b86211c8c56f5da9e183501d0c54d37d3e8e9b7c
taiming <taiming@bridgewell.com>
2021-06-14 21:17:08 +0800

    Styling img in parser

--- src/components/htmlParser/index.js
+++ src/components/htmlParser/index.js
@@ -1,10 +1,26 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 const createMarkup = (htmlString) => ({ __html: htmlString });
 
-const HtmlParser = ({ htmlString }) => (
-  <div dangerouslySetInnerHTML={createMarkup(htmlString)} />
-);
+const useStyles = makeStyles({
+  root: {
+    '& img': {
+      maxWidth: '100% !important',
+    },
+  },
+});
+
+const HtmlParser = ({ htmlString }) => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={classes.root}
+      dangerouslySetInnerHTML={createMarkup(htmlString)}
+    />
+  );
+};
 
 export default HtmlParser;
